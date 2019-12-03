c3b82e7e4ccf0554bebd60cc0d61f00988d47f04
Eyal.Kapah <eykapah@microsoft.com>
2019-12-03 10:11:07 +0200

    work on settings

--- EuroMobile/EuroMobile/ViewModels/SettingsPageViewModel.cs
+++ EuroMobile/EuroMobile/ViewModels/SettingsPageViewModel.cs
@@ -8,9 +8,26 @@ namespace EuroMobile.ViewModels
 {
     public class SettingsPageViewModel : BindableBase
     {
+        private string _fullName;
+
+        public string FullName
+        {
+            get => _fullName;
+            set => SetProperty(ref _fullName, value);
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
         public SettingsPageViewModel()
         {
-
+            FullName = "Eyal Kapah";
+            Email = "eyalk@nomail.com";
         }
     }
 }
